2aa2c2755d4d8bc4c39b9d14d719d7a9d8145800
Puddle Jumpers <frc7332@gmail.com>
2025-04-23 15:17:12 -0700

    code from comp

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,65 +17,89 @@ import edu.wpi.first.wpilibj.Timer;
 
 /** This is a demo program showing how to use Mecanum control with the MecanumDrive class. */
 public class Robot extends TimedRobot {
-  private static final int kFrontLeftChannel = 2;
-  private static final int kRearLeftChannel = 3;
-  private static final int kFrontRightChannel = 1;
-  private static final int kRearRightChannel = 0;
+  private static final int kFrontLeftChannel = 1;
+  private static final int kRearLeftChannel = 0;
+  private static final int kFrontRightChannel = 2;
+  private static final int kRearRightChannel = 3;
 
   XboxController m_controller = new XboxController(0);
 
   Spark m_intakeLeft = new Spark(0);
   Spark m_intakeRight = new Spark(1);
-  Spark m_deepcage = new Spark(2);
+  Spark m_deepcage = new Spark(5);
 
   PWMSparkMax m_elevator1 = new PWMSparkMax(3);
   PWMSparkMax m_elevator2 = new PWMSparkMax(4);
 
-  WPI_VictorSPX frontLeft = new WPI_VictorSPX(kFrontLeftChannel);
-  WPI_VictorSPX rearLeft = new WPI_VictorSPX(kRearLeftChannel);
-  WPI_VictorSPX frontRight = new WPI_VictorSPX(kFrontRightChannel);
-  WPI_VictorSPX rearRight = new WPI_VictorSPX(kRearRightChannel);
+  WPI_VictorSPX a = new WPI_VictorSPX(kRearRightChannel); //rear right, 3
+  WPI_VictorSPX b = new WPI_VictorSPX(kFrontRightChannel); //front right, 2
+  WPI_VictorSPX c = new WPI_VictorSPX(kFrontLeftChannel); // front left, 1
+  WPI_VictorSPX d = new WPI_VictorSPX(kRearLeftChannel); //rear left, 0
 
-  private final MecanumDrive m_robotDrive = new MecanumDrive(frontLeft, rearLeft, frontRight, rearRight);
+  private final MecanumDrive m_robotDrive = new MecanumDrive(a, b, c, d);
 
   Timer timer = new Timer();
 
   /** Called once at the beginning of the robot program. */
   @Override
   public void robotInit() {
-    CameraServer.startAutomaticCapture();
-    frontRight.setInverted(true);
-    rearRight.setInverted(true);
+    CameraServer.startAutomaticCapture(0);
+    CameraServer.startAutomaticCapture(1);
+   // d.setInverted(true);
   }
 
 
   @Override
   public void autonomousInit(){
+    a.setInverted(true);
+    c.setInverted(true);
+    d.setInverted(false);
+    b.setInverted(false);
+  /*   frontLeft.setInverted(true);
+    rearLeft.setInverted(true);
+    frontRight.setInverted(false); */
     timer.restart();
     timer.start();
+
+
   }
 
 
   @Override
   public void autonomousPeriodic(){
     double time = timer.get();
-    if (time < 2.0){
-      m_robotDrive.driveCartesian(0.0, 0.5, 0.0);
-    }
-    /*
-     * else if (time < 4.0){
-     * m_robotDrive.driveCartesian(xSpeed, ySpeed, zRotation); //turn maybe??
-     * }
-     */
+       if (time < 2.0){
+      m_robotDrive.driveCartesian(0.0, -0.4, 0.0);
+    }  
+     else if (time > 2.0 && time < 3){
+      m_robotDrive.driveCartesian(0, 0, 0);
+      m_elevator1.set(-.5);
+      m_elevator2.set(-.5); 
+    } 
+        else if (time > 3 && time < 5){
+        m_elevator1.set(0);
+        m_elevator2.set(0);
+        m_intakeLeft.set(0.6);
+        m_intakeRight.set(0.25);
+      } 
     else {
       m_robotDrive.driveCartesian(0.0, 0.0, 0.0);
-    }
+      m_elevator1.set(0);
+      m_elevator2.set(0);
+      m_intakeLeft.set(0);
+      m_intakeRight.set(0);
+    } 
   }
 
 
   @Override
   public void teleopInit(){
-    //what do i even put here ;-;
+    //what do i even put here ;-; 
+    b.setInverted(false);
+    d.setInverted(true);
+    a.setInverted(false);
+    c.setInverted(true);
+
   }
 
 
@@ -87,76 +111,127 @@ public class Robot extends TimedRobot {
     double ySpeed = m_controller.getRightX();
     double zRotation = m_controller.getLeftX();
 
+    boolean revIntake = m_controller.getXButton();
     boolean intakeButton = m_controller.getYButton(); 
-    boolean elevatorButton1 = m_controller.getAButtonPressed(); 
-    boolean elevatorButton2 = m_controller.getBButtonPressed();
-    boolean revelevatorButton = m_controller.getXButton();
-    boolean deepcageButton = m_controller.getStartButtonPressed(); 
-    boolean troughButton = m_controller.getBackButtonPressed();
+    boolean elevatorUp = m_controller.getAButton();
+    boolean elevatorDown = m_controller.getBButton();
 
-    m_robotDrive.driveCartesian(xSpeed * 0.25, -ySpeed * 0.25, zRotation * 0.25);
+   // boolean troughButton = m_controller.getRightBumperButton();
+
+    double deepCageUp = m_controller.getLeftTriggerAxis();
+    double deepCageDown = m_controller.getRightTriggerAxis();
+
+  //  int deepcageButton = m_controller.getPOV(); //manual elevator up
+  //  int revdeepcageButton = m_controller.getPOV(); //manual elevator down
+
+    //boolean elevatorUp = m_controller.povUp(null).getAsBoolean();
+    //boolean elevatorDown = m_controller.povDown(null).getAsBoolean();
+
+    m_robotDrive.driveCartesian(xSpeed * 0.6, -ySpeed * 0.6, zRotation * 0.6);
+
+    /*if (revIntake == 1){
+      m_intakeLeft.set(-0.5);
+      m_intakeRight.set(0.5);
+    } else  {
+      m_intakeLeft.set(0);
+      m_intakeRight.set(0);
+    } */
+
+   /*  if (manElevatorDown == 180){
+      m_elevator1.set(0.5);
+      m_elevator2.set(0.5);
+    } else {
+      m_elevator1.set(0);
+      m_elevator2.set(0);
+    }
+    if (manElevatorUp == 0){
+      m_elevator1.set(-0.5);
+      m_elevator2.set(-0.5);
+    } else{
+     m_elevator1.set(0);
+     m_elevator2.set(0);
+     }   */
+    
     
     if (intakeButton) { //might need to change direction
-      m_intakeLeft.set(0.75);  
-      m_intakeRight.set(-1);
+      m_intakeLeft.set(0.4);  
+      m_intakeRight.set(0.4);
+    }
+    else if (revIntake) { //might need to change direction
+      m_intakeLeft.set(0.6);  
+      m_intakeRight.set(0.25);
     }
     else {
       m_intakeLeft.set(0);
       m_intakeRight.set(0);
     }
    
-    if (elevatorButton1) {
-      timer.reset();
-      timer.start();
-      if (timer.get() < 3.0) {
-      m_elevator1.set(0.75);
-      m_elevator2.set(-0.75);
-      }
-    } else if (revelevatorButton) {
-     /*/ timer.reset();
-      timer.start();
-      if (timer.get() < 0.5){ */
-      m_elevator1.set(-0.75);
-      m_elevator2.set(-0.75);
+     if (elevatorUp){ 
+      m_elevator1.set(-0.5);
+      m_elevator2.set(-0.5);
+    } else if (elevatorDown){
+      m_elevator1.set(0.5);
+      m_elevator2.set(0.5);
     } else {
       m_elevator1.set(0);
       m_elevator2.set(0);
-    }
+    } 
 
-    if (elevatorButton2){
+     /*   if (troughButton = true){
+     timer.reset();
+     timer.start();
+      if (timer.get() < 3.0 && timer.get() > 1.0){
+      m_elevator1.set(-0.5);
+      m_elevator2.set(-0.5);
+  }
+      else if (timer.get() > 2.0 && timer.get() < 4.0){
+      m_elevator1.set(0);
+      m_elevator2.set(0);
+      m_intakeRight.set(.4);
+      m_intakeLeft.set(.4);
+      } else {
+       m_elevator1.set(0);
+       m_elevator2.set(0);
+       m_intakeRight.set(0);
+       m_intakeLeft.set(0);
+       }
+  } */
+     
+ //DEEPCAGE NEGATIVE NUMBERS = UP
+      if (deepCageDown == 1){
+      m_deepcage.set(0.25);
+    } else if (deepCageUp == 1){
+      m_deepcage.set(-0.25);
+    } else {
+      m_deepcage.set(0);
+    }  
+  }
+  /*  if (elevatorButton1) {
+      timer.reset();
+      timer.start();
+      if (timer.get() < 3.0) {
+      m_elevator1.set(-0.75);
+      m_elevator2.set(-0.75);
+      }
+    }  else if (elevatorButton2){
       timer.reset();
       timer.start();
       if (timer.get() < 3.5 ){
-        m_elevator1.set(0.75); 
-      }
-    } else if (revelevatorButton) {
-       /*  timer.reset();
-        timer.start();
-        if (timer.get() < 1 ) {  */
-        m_elevator1.set(-0.75);
+        m_elevator1.set(-0.75); 
         m_elevator2.set(-0.75);
-    } else {
-        m_elevator1.set(0);
-        m_elevator2.set(0);
-    }
-
-    if (deepcageButton){
-      m_deepcage.set(0.75);
-    } else {
-      m_deepcage.set(0);
-    }
-
-    if (troughButton){
+      }
+    }  else if (troughButton){
       timer.reset();
       timer.start();
       if(timer.get() < 2.0){
-        m_elevator1.set(0.75);
-        m_elevator2.set(0.75);
-      }
-    } else {
+        m_elevator1.set(-0.75);
+        m_elevator2.set(-0.75); 
+      
+     else {
         m_elevator1.set(0);
-        m_elevator2.set(0);
-    }
-
-  }
-}
+        m_elevator2.set(0); }*/
+    
+    
+  
+    } 
+  
